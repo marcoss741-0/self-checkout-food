66340c1172dd499c2c22cb46c962374ff607ee4e
MARCOS MARTINS DA SILVA SANTOS <marcossan741@icloud.com>
2025-05-24 16:45:56 -0300

    Add new logo and take-away images to the public directory
    - Added logo.png (3,198 bytes) for branding purposes.
    - Added take-away.png (6,318 bytes) to enhance the visual content of the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,66 @@
+"use client";
+
+import Link from "next/link";
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
+import { Card, CardContent } from "./_components/ui/card";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="flex h-screen flex-col items-center justify-center px-6 pt-10">
+        {/* LOGO E TITULO */}
+        <div className="flex flex-col items-center gap-2">
+          <Image
+            src={"/logo.png"}
+            alt={"Logo do aplicativo Self-Choice"}
+            width={82}
+            height={82}
+          />
+          <h2 className="font-semibold">{"Self Choice"}</h2>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {/* BEM VINDO */}
+        <div className="space-y-2 pt-24 text-center">
+          <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+          <p className="opacity-55">
+            Escolha como prefere aproveitar sua refeição. Estamos aqui para
+            oferecer praticidade e sabor em cada detalhe!
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-4 pt-14">
+          <Card>
+            <CardContent className="flex flex-col items-center gap-8 py-8">
+              <div className="relative h-[80px] w-[80px]">
+                <Image
+                  src="/dine-in.png"
+                  fill
+                  alt="Dine In"
+                  className="object-contain"
+                />
+              </div>
+              <Button variant="secondary" className="rounded-full" asChild>
+                <Link href={`#`}>Comer aqui</Link>
+              </Button>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardContent className="flex flex-col items-center gap-8 py-8">
+              <div className="relative h-[80px] w-[80px]">
+                <Image
+                  src="/take-away.png"
+                  fill
+                  alt="Take Away"
+                  className="object-contain"
+                />
+              </div>
+              <Button variant="secondary" className="rounded-full" asChild>
+                <Link href={`#`}>Para viagem</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
